fefd1c54e186e789ed74255319f0b0ffcf2a4354
albertocirilli <37694694+albertocirilli@users.noreply.github.com>
2022-01-16 09:50:36 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo January 14th 2022' });
+  res.render('index', { title: 'Cloud Native demo January 16th 2022' });
 });
 
 module.exports = router;
